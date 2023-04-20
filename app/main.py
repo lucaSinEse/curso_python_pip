5e42c44146f5ab5d426394e41f2a8259032748fc
Luca Cussino <lucacussino@hotmail.com>
2023-04-20 15:24:27 -0300

    modificacion de la app

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ import read_csv
 import charts
 
 def run():
-  data = read_csv.read_csv('./app/data.csv')
+  data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-  '''
+  
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
@@ -17,8 +17,8 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
-  '''
+    charts.generate_bar_chart(country['Country'], labels, values)
+
 
 if __name__ == '__main__':
   run()
