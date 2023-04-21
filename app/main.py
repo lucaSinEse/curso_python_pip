6db603e3321079cbff8c83fe2e0847168f8f2281
Luca Cussino <lucacussino@hotmail.com>
2023-04-20 22:10:31 -0300

    solicitudes http, pandas y fastAPI

--- app/main.py
+++ app/main.py
@@ -1,17 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-  
-  country = input('Type Country => ')
+  '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country'].values
+  porcentajes = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, porcentajes)
 
+  country = input('Type Country => ')
+  data = read_csv.read_csv('data.csv')
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
